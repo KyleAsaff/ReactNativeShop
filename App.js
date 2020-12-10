e63363af47bcce1fd1b26d392666281efd00af6e
Kyle Asaff <kyleasaff@pixleeteam.com>
2020-12-10 12:47:58 -0400

    packages

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import productsReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  products: productsReducer
+});
 
 export default function App() {
   return (
+    <Provider store={store}>
     <View>
       <Text>A new app!</Text>
     </View>
+    </Provider>
   );
 }
