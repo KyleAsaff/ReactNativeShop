46b7c10a104a3592f0e0fab943c4456138bfda68
Kyle Asaff <kyleasaff@pixleeteam.com>
2020-12-10 13:25:01 -0400

    all products

--- App.js
+++ App.js
@@ -4,17 +4,18 @@ import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import productsReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
-  products: productsReducer
+  products: productsReducer,
 });
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
     <Provider store={store}>
-    <View>
-      <Text>A new app!</Text>
-    </View>
+      <ShopNavigator />
     </Provider>
   );
 }
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -2,9 +2,7 @@ import PRODUCTS from '../../data/dummy-data';
 
 const initialState = {
   availableProducts: PRODUCTS,
-  userProducts: PRODUCTS.filter(product => product.ownerId === 'u1'),
+  userProducts: PRODUCTS.filter((product) => product.ownerId === 'u1'),
 };
 
-export default(state = initialStage, action) => {
-  return state;
-};
+export default (state = initialState, action) => state;
