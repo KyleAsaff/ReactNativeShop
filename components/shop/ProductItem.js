150cf98eda24505194ca2f17226f54fb36cd5fc0
Kyle Asaff <kyleasaff@pixleeteam.com>
2020-12-11 17:09:55 -0400

    finished v1 of shop app before input validations

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import {
-  View, Text, Image, StyleSheet, Button, TouchableOpacity, TouchableNativeFeedback, Platform,
+  View, Text, Image, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform,
 } from 'react-native';
 
-import Colors from '../../constants/Colors';
+import Card from '../UI/Card';
 
 const styles = StyleSheet.create({
   product: {
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: 'white',
     height: 300,
     margin: 20,
   },
@@ -31,7 +24,7 @@ const styles = StyleSheet.create({
   details: {
     fontFamily: 'open-sans',
     alignItems: 'center',
-    height: '15%',
+    height: '17%',
     padding: 10,
   },
   title: {
@@ -48,7 +41,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    height: '25%',
+    height: '23%',
     paddingHorizontal: 20,
   },
   touchable: {
@@ -63,12 +56,12 @@ const ProductItem = (props) => {
     TouchableCmp = TouchableNativeFeedback;
   }
   const {
-    image, title, price, onViewDetail, onAddToCart,
+    image, title, price, onSelect,
   } = props;
   return (
-    <View style={styles.product}>
+    <Card style={styles.product}>
       <View style={styles.touchable}>
-        <TouchableCmp onPress={onViewDetail} useForeground>
+        <TouchableCmp onPress={onSelect} useForeground>
           <View>
             <View style={styles.imageContainer}>
               <Image style={styles.image} source={{ uri: image }} />
@@ -81,13 +74,12 @@ const ProductItem = (props) => {
               </Text>
             </View>
             <View style={styles.actions}>
-              <Button color={Colors.primary} title="View Details" onPress={onViewDetail} />
-              <Button color={Colors.primary} title="To Cart" onPress={onAddToCart} />
+              {props.children}
             </View>
           </View>
         </TouchableCmp>
       </View>
-    </View>
+    </Card>
   );
 };
 
--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {
   View, Text, FlatList, Button, StyleSheet,
 } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
+import Card from '../../components/UI/Card';
+import * as cartActions from '../../store/actions/cart';
+import * as orderActions from '../../store/actions/orders';
 
 const CartScreen = (props) => {
   const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
+  const dispatch = useDispatch();
   const cartItems = useSelector((state) => {
     const transformedCartItems = [];
     for (const key in state.cart.items) {
@@ -20,26 +24,29 @@ const CartScreen = (props) => {
         sum: state.cart.items[key].sum,
       });
     }
-    return transformedCartItems;
+    return transformedCartItems.sort((a, b) => (a.productId > b.productId ? 1 : -1));
   });
 
   return (
     <View style={styles.screen}>
-      <View style={styles.summary}>
+      <Card style={styles.summary}>
         <Text style={styles.summaryText}>
           Total:
           {' '}
           <Text style={styles.amount}>
             $
-            {cartTotalAmount.toFixed(2)}
+            {Math.round((cartTotalAmount.toFixed(2) * 100) / 100)}
           </Text>
         </Text>
         <Button
           color={Colors.accent}
           title="Order Now"
           disabled={cartItems.length === 0}
+          onPress={() => {
+            dispatch(orderActions.addOrder(cartItems, cartTotalAmount));
+          }}
         />
-      </View>
+      </Card>
       <FlatList
         data={cartItems}
         keyExtractor={(item) => item.productId}
@@ -48,7 +55,7 @@ const CartScreen = (props) => {
             quantity={itemData.item.quantity}
             title={itemData.item.productTitle}
             amount={itemData.item.sum}
-            onRemove={() => {}}
+            onRemove={() => { dispatch(cartActions.removeFromCart(itemData.item.productId)); }}
           />
         )}
       />
@@ -66,13 +73,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     marginBottom: 20,
     padding: 10,
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: 'white',
   },
   summaryText: {
     fontFamily: 'open-sans-bold',
@@ -82,5 +82,9 @@ const styles = StyleSheet.create({
     color: Colors.primary,
   },
 });
+
+CartScreen.navigationOptions = {
+  headerTitle: 'Your Cart',
+};
 
 export default CartScreen;
