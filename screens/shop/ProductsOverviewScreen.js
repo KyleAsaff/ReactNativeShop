a976a213a252226ffcd3cb4532d8078c7d256fc7
Kyle Asaff <kyleasaff@pixleeteam.com>
2020-12-11 11:45:28 -0400

    171

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList, Platform } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
+import HeaderButton from '../../components/UI/HeaderButton';
 
 const ProductsOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
@@ -33,8 +35,16 @@ const ProductsOverviewScreen = (props) => {
   );
 };
 
-ProductsOverviewScreen.navigationOptions = {
+ProductsOverviewScreen.navigationOptions = (navData) => ({
   headerTitle: 'All Products',
-};
+  headerRight:
+  <HeaderButtons HeaderButtonComponent={HeaderButton}>
+    <Item
+      title="Cart"
+      iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+      onPress={() => { navData.navigation.navigate('Cart'); }}
+    />
+  </HeaderButtons>,
+});
 
 export default ProductsOverviewScreen;
