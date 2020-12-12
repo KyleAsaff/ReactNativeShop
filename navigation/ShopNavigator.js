7b9b4748fcbe83dacecab037bbececeda3917ee9
Kyle Asaff <kyleasaff@pixleeteam.com>
2020-12-12 11:52:38 -0400

    get switchnavigator to work

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -8,6 +8,7 @@ import {
 import {
   createDrawerNavigator,
 } from 'react-navigation-drawer';
+import { createSwitchNavigator } from 'react-navigation-4';
 import { Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -18,6 +19,7 @@ import OrdersScreen from '../screens/shop/OrdersScreen';
 import UserProductsScreen from '../screens/user/UserProductsScreen';
 import Colors from '../constants/Colors';
 import EditProductScreen from '../screens/user/EditProductScreen';
+import AuthScreen from '../screens/user/AuthScreen';
 
 const defaultNavOptions = {
   headerStyle: {
@@ -102,4 +104,18 @@ const ShopNavigator = createDrawerNavigator(
   },
 );
 
-export default createAppContainer(ShopNavigator);
+const AuthNavigator = createStackNavigator(
+  {
+    Auth: AuthScreen,
+  },
+  {
+    defaultNavigationOptions: defaultNavOptions,
+  },
+);
+
+const MainNavigator = createSwitchNavigator({
+  Auth: AuthNavigator,
+  Shop: ShopNavigator,
+});
+
+export default createAppContainer(MainNavigator);
